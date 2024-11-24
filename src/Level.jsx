68b06403d9d8ba8bc7912b8acc0f129fa5b0888d
Po-Cheng Yeh <blp100x@hotmail.com>
2024-11-24 18:57:06 +0800

    feat: add hamburger reward to BlockEnd component

--- src/Level.jsx
+++ src/Level.jsx
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { RigidBody } from "@react-three/rapier";
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 
@@ -53,6 +54,12 @@ function BlockStart({ position = [0, 0, 0] }) {
  * @returns {JSX.Element} A Three.js group containing the floor for the ending block.
  */
 function BlockEnd({ position = [0, 0, 0] }) {
+  const hamburger = useGLTF("./hamburger.glb");
+
+  hamburger.scene.children.forEach((mesh) => {
+    mesh.castShadow = true;
+  });
+
   return (
     <group position={position}>
       {/* Floor */}
@@ -63,6 +70,15 @@ function BlockEnd({ position = [0, 0, 0] }) {
         scale={[4, 0.2, 4]}
         receiveShadow
       />
+      <RigidBody
+        type="fixed"
+        colliders="hull"
+        position={[0, 0.25, 0]}
+        restitution={0.2}
+        friction={0}
+      >
+        <primitive object={hamburger.scene} scale={0.2} />
+      </RigidBody>
     </group>
   );
 }
