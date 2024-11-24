defd4f6f400710fe3baaef413d56479d3e5d6d8b
Po-Cheng Yeh <blp100x@hotmail.com>
2024-11-24 22:33:12 +0800

    feat: add keyboard controls for movement direction

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,8 +1,50 @@
 import { RigidBody } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
+import { useKeyboardControls } from "@react-three/drei";
+import { useRef } from "react";
 
 export default function Player() {
+  const body = useRef();
+  const [subscribeKeys, getKeys] = useKeyboardControls();
+
+  useFrame((state, delta) => {
+    const { forward, backward, leftward, rightward, jump } = getKeys();
+
+    const impulse = { x: 0, y: 0, z: 0 };
+    const torque = { x: 0, y: 0, z: 0 };
+
+    const impulseStrength = 1 * delta;
+    const torqueStrength = 1 * delta;
+
+    if (forward) {
+      impulse.z -= impulseStrength;
+      torque.x -= torqueStrength;
+    }
+
+    if (rightward) {
+      impulse.x += impulseStrength;
+      torque.z -= torqueStrength;
+    }
+
+    if (backward) {
+      impulse.z += impulseStrength;
+      torque.x += torqueStrength;
+    }
+
+    if (leftward) {
+      impulse.x -= impulseStrength;
+      torque.z += torqueStrength;
+    }
+
+    if (body.current) {
+      body.current.applyImpulse(impulse);
+      body.current.applyTorqueImpulse(torque);
+    }
+  });
+
   return (
     <RigidBody
+      ref={body}
       canSleep={false}
       colliders="ball"
       restitution={0.2}
