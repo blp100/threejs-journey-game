7c222ccbbcfdc48de885703c28f34e271b284a6a
Po-Cheng Yeh <blp100x@hotmail.com>
2024-11-24 19:48:48 +0800

    feat: add physics floor to Bounds to prevent ball falling down to void

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { RigidBody } from "@react-three/rapier";
+import { CuboidCollider, RigidBody } from "@react-three/rapier";
 import { useMemo, useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
@@ -282,6 +282,12 @@ function Bounds({ length = 1 }) {
           material={wallMaterial}
           scale={[4, 1.5, 0.3]}
           receiveShadow
+        />
+        <CuboidCollider
+          args={[2, 0.1, 2 * length]}
+          position={[0, -0.1, -(length * 2) + 2]}
+          restitution={0.2}
+          friction={1}
         />
       </RigidBody>
     </>
