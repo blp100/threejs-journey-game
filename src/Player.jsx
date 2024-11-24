185421e5e2ebfbb88c99984b00cf389ab83b80cf
Po-Cheng Yeh <blp100x@hotmail.com>
2024-11-24 23:56:03 +0800

    feat: implement jump method for player (ball) using raycasting

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,14 +1,31 @@
-import { RigidBody } from "@react-three/rapier";
+import { useRapier, RigidBody } from "@react-three/rapier";
 import { useFrame } from "@react-three/fiber";
 import { useKeyboardControls } from "@react-three/drei";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Player() {
   const body = useRef();
   const [subscribeKeys, getKeys] = useKeyboardControls();
+  const { rapier, world } = useRapier();
+
+  const jump = (value) => {
+    const origin = body.current.translation();
+    origin.y -= 0.31;
+    const direction = { x: 0, y: -1, z: 0 };
+    const ray = new rapier.Ray(origin, direction);
+    const hit = world.castRay(ray, 10, true);
+
+    if (hit.timeOfImpact < 0.15)
+      body.current.applyImpulse({ x: 0, y: 0.5, z: 0 });
+  };
+
+  useEffect(() => {
+    const unsubscribeJump = subscribeKeys((state) => state.jump, jump);
+    return () => unsubscribeJump();
+  }, []);
 
   useFrame((state, delta) => {
-    const { forward, backward, leftward, rightward, jump } = getKeys();
+    const { forward, backward, leftward, rightward } = getKeys();
 
     const impulse = { x: 0, y: 0, z: 0 };
     const torque = { x: 0, y: 0, z: 0 };
