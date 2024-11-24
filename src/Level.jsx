aceab23323601f6a6264560e0e9ef2b294b15581
Po-Cheng Yeh <blp100x@hotmail.com>
2024-11-24 13:04:34 +0800

    feat: add rotation movement on spinner

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+import { RigidBody } from "@react-three/rapier";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 
@@ -18,8 +21,8 @@ const wallMaterial = new THREE.MeshStandardMaterial({
 /**
  * BlockStart Component
  *
- * This component renders a 3D block group in a Three.js scene. It includes:
- * - A floor mesh with a box geometry that acts as the base.
+ * This component renders a simple 3D block group in a Three.js scene.
+ * It consists of a floor mesh with custom geometry and material.
  *
  * @param {Object} props - Component props.
  * @param {Array<number>} props.position - The position of the block group in 3D space.
@@ -41,7 +44,18 @@ function BlockStart({ position = [0, 0, 0] }) {
   );
 }
 
+
 function BlockSpinner({ position = [0, 0, 0] }) {
+  const obstacle = useRef();
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+
+    const rotation = new THREE.Quaternion();
+    rotation.setFromEuler(new THREE.Euler(0, time, 0));
+    if (obstacle.current) obstacle.current.setNextKinematicRotation(rotation);
+  });
+
   return (
     <group position={position}>
       {/* Floor */}
@@ -52,13 +66,21 @@ function BlockSpinner({ position = [0, 0, 0] }) {
         scale={[4, 0.2, 4]}
         receiveShadow
       />
-      <mesh
-        geometry={boxGeometry}
-        material={obstacleMaterial}
-        scale={[3.5, 0.3, 0.3]}
-        castShadow
-        receiveShadow
-      />
+      <RigidBody
+        ref={obstacle}
+        type="kinematicPosition"
+        position={[0, 0.3, 0]}
+        restitution={0.2}
+        friction={0}
+      >
+        <mesh
+          geometry={boxGeometry}
+          material={obstacleMaterial}
+          scale={[3.5, 0.3, 0.3]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
     </group>
   );
 }
